d7b96456f71962533129298511684fddefc2f3b6
Christian Nickolaou <christian.j.nick@gmail.com>
2017-10-09 17:33:26 -0400

    added Exercise class to hold the information for the workouts

---
+++ src/helper/Exercise.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author cjwn
+ */
+public class Exercise {
+    private String name;
+    private String category;
+    private String description;
+    
+    public Exercise(String exerciseName, String exerciseCat, String exerciseDesc) {
+        name = exerciseName;
+        category = exerciseCat;
+        description = exerciseDesc;
+    }
+   
+}
